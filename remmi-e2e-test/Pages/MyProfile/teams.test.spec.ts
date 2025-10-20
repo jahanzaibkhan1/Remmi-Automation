73be2861a696fd4e485d027c7abcad09c7bda6c2
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-10-20 15:25:39 +0500

    Verify no results message appears for invalid team name

--- remmi-e2e-test/Pages/MyProfile/teams.test.spec.ts
+++ remmi-e2e-test/Pages/MyProfile/teams.test.spec.ts
@@ -16,6 +16,12 @@ async function SearchForExistingTeam() {
   await profile.SearchForExistingTeam('Hina Team');
 }
 
+async function SearchForInvalidTeam() {
+  await profile.navigateToProfilePage();
+  await profile.SearchForInvalidTeam('fjjs jjs');
+}
+
+
 // ----------- Tests -----------
 
 test.describe('Teams Tab Tests - Remmi E2E', () => {
@@ -31,4 +37,18 @@ test.describe('Teams Tab Tests - Remmi E2E', () => {
 
     await SearchForExistingTeam();
   });
+
+  test('Test case 2: Verify no results message appears for invalid team name.', async ({ page }) => {
+    login = new LoginActions(page);
+    profile = new MyProfileActions(page);
+
+    if (!operationManager.email || !operationManager.password || !operationManager.otpSecret) {
+      test.skip(true, 'Skipping login tests: missing environment credentials');
+    }
+
+    await login.login(operationManager.email!, operationManager.password!, operationManager.otpSecret!);
+
+    await SearchForInvalidTeam();
+  });
+
 });
