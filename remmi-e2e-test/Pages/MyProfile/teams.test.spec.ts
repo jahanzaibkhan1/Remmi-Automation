2e0f10691661e0d33d0013d3e7d95cc2bbcefd27
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-10-22 17:35:38 +0500

    Verify successful team creation with all valid details

--- remmi-e2e-test/Pages/MyProfile/teams.test.spec.ts
+++ remmi-e2e-test/Pages/MyProfile/teams.test.spec.ts
@@ -292,5 +292,16 @@ test.describe('Teams Tab Tests - Remmi E2E', () => {
     await profile.navigateToProfilePage();
     await profile.VerifyTeamCreationWithoutLeader('QA Tester', 'Dawood Ahmad');
   });
+  test('Test case 21: Verify successful team creation with all valid details.', async ({ page }) => {
+    const login = new LoginActions(page);
+    const profile = new MyProfileActions(page);
 
+    if (!operationManager.email || !operationManager.password || !operationManager.otpSecret) {
+      test.skip(true, 'Skipping login tests: missing environment credentials');
+    }
+
+    await login.login(operationManager.email!, operationManager.password!, operationManager.otpSecret!);
+    await profile.navigateToProfilePage();
+    await profile.VerifyTeamCreationWithValidDetails('QA Tester', 'Dawood Ahmad', 'Daud Ahmad');
+  });
 });
