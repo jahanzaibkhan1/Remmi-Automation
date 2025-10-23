a80caed186e5b6629b98e8c0547672d1133edf01
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2025-10-23 14:44:24 +0500

    Verify failed team creation doesn’t reflect in list

--- remmi-e2e-test/Pages/MyProfile/teams.test.spec.ts
+++ remmi-e2e-test/Pages/MyProfile/teams.test.spec.ts
@@ -341,4 +341,16 @@ test.describe('Teams Tab Tests - Remmi E2E', () => {
     await profile.navigateToProfilePage();
     await profile.verifyNewTeamAppearsInListAndDropdown('QA Tester', 'Dawood Ahmad', 'Daud Ahmad');
   });
+  test('Test case 25: Verify failed team creation doesn’t reflect in list.', async ({ page }) => {
+    const login = new LoginActions(page);
+    const profile = new MyProfileActions(page);
+
+    if (!operationManager.email || !operationManager.password || !operationManager.otpSecret) {
+      test.skip(true, 'Skipping login tests: missing environment credentials');
+    }
+
+    await login.login(operationManager.email!, operationManager.password!, operationManager.otpSecret!);
+    await profile.navigateToProfilePage();
+    await profile.verifyEmptySpacesTeamNameDoesNotReflectInList('QA Tester', 'Dawood Ahmad', 'Daud Ahmad');
+  });
 });
