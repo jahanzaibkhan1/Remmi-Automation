272de7818eab2ac8fc7e5c510fc811986c9f8cfc
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-10-24 13:38:59 +0500

    Verify unsaved popup data lost on refresh.

--- remmi-e2e-test/Pages/MyProfile/teams.test.spec.ts
+++ remmi-e2e-test/Pages/MyProfile/teams.test.spec.ts
@@ -544,4 +544,16 @@ test.describe('Teams Tab Tests - Remmi E2E', () => {
     await profile.navigateToProfilePage();
     await profile.VerifyTeamCreationWithValidDetails('QA Tester', 'Dawood Ahmad', 'Daud Ahmad');
   });
+  test('Test case 41: Verify unsaved popup data lost on refresh.', async ({ page }) => {
+    const login = new LoginActions(page);
+    const profile = new MyProfileActions(page);
+
+    if (!operationManager.email || !operationManager.password || !operationManager.otpSecret) {
+      test.skip(true, 'Skipping login tests: missing environment credentials');
+    }
+
+    await login.login(operationManager.email!, operationManager.password!, operationManager.otpSecret!);
+    await profile.navigateToProfilePage();
+    await profile.VerifyUnsavedPopupDataLostOnRefresh('QA Tester', 'Dawood Ahmad', 'Daud Ahmad');
+  });
 });
