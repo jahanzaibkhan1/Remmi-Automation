643390be88920a990edfc7333e5935ca61f1270f
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-11-03 14:16:25 +0500

    Verify the sort functionality

--- remmi-e2e-test/Pages/MyProfile/Association.spec.ts
+++ remmi-e2e-test/Pages/MyProfile/Association.spec.ts
@@ -138,4 +138,13 @@ test.describe('My Profile Associations Tab Tests - Remmi E2E', () => {
         await profile.navigateToProfilePage();
         await profile.verifyInitialProjectSelection(["Hina's Project", "askari center"]);
       });
+      test('Test 14: Verify the sort functionality', async ({ page }) => {
+        const login = new LoginActions(page);
+        const profile = new MyProfileActions(page);
+
+        await login.login(OprationManager.email!, OprationManager.password!, OprationManager.otpSecret!);
+
+        await profile.navigateToProfilePage();
+        await profile.verifyAssocitionSortingList();
+      });
 })
