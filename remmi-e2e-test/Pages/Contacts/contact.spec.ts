375f9dfe25698389e684fa716b40981f910264d3
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-11-04 16:37:04 +0500

    Searching for an existing listing

---
+++ remmi-e2e-test/Pages/Contacts/contact.spec.ts
@@ -0,0 +1,21 @@
+import { test } from '@playwright/test';
+import { ContactActions } from './contactAction';
+import { LoginActions } from '../Login/LoginAction';
+import { LoginUsers } from '../../fixture/test-data';
+
+const OprationManager = LoginUsers.manager;
+const salesAgent = LoginUsers.sales;
+const admin = LoginUsers.admin;
+
+
+test.describe('Contacts side Menu Tests - Remmi E2E', () => {
+
+    test('Test 1: Searching for an existing listing', async ({ page }) => {
+        const login = new LoginActions(page);
+        const contact = new ContactActions(page);
+    
+        await login.login(OprationManager.email!, OprationManager.password!, OprationManager.otpSecret!);
+        await contact.NavigateToContacts();
+        await contact.searchExistingContact('Hina Test')
+      });
+});
