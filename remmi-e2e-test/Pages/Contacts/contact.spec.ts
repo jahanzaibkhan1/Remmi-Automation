9b2993c9164ae47433af16af4b417fe3611fa0d2
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2025-11-11 09:41:24 +0500

    Verify selecting a tag in dropdown filters contacts correctly

--- remmi-e2e-test/Pages/Contacts/contact.spec.ts
+++ remmi-e2e-test/Pages/Contacts/contact.spec.ts
@@ -538,4 +538,16 @@ test.describe('Contacts side Menu Tests - Remmi E2E', () => {
     await contact.verifySearchingAndLoadingMoreContacts();
   });
 
+test('Test 43: Verify selecting a tag in dropdown filters contacts correctly', async ({ page }) => {
+  const login = new LoginActions(page);
+  const contact = new ContactActions(page);
+
+  await login.login(
+    OprationManager.email!,
+    OprationManager.password!,
+    process.env.E2E_MANAGER_OTP_SECRET!
+  );
+  await contact.verifyTagDropdownFilter('Agent');
+});
+
 });
