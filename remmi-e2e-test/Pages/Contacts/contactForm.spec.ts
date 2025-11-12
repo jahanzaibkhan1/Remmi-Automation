cc6f2ac68741eb702b7d55b9458051e0274782fe
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-11-12 13:11:00 +0500

    Verify that an email or phone field can be deleted

--- remmi-e2e-test/Pages/Contacts/contactForm.spec.ts
+++ remmi-e2e-test/Pages/Contacts/contactForm.spec.ts
@@ -181,5 +181,17 @@ test.describe('Contacts side Menu Tests - Remmi E2E', () => {
     );
     await contact.verifyAddPhoneField();
   });
+
+  test('Test 15: Verify that an email or phone field can be deleted', async ({ page }) => {
+    const login = new LoginActions(page);
+    const contact = new ContactActions(page);
+
+    await login.login(
+      OprationManager.email!,
+      OprationManager.password!,
+      process.env.E2E_MANAGER_OTP_SECRET!
+    );
+    await contact.verifyDeleteEmailOrPhoneField();
+  });
 });
   
