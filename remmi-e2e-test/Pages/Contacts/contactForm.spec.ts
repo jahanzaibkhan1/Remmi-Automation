69ff72692d26718a719cc79b4947e5611635ad35
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2025-11-12 10:17:41 +0500

    Verify contact initials placeholder displays correctly

--- remmi-e2e-test/Pages/Contacts/contactForm.spec.ts
+++ remmi-e2e-test/Pages/Contacts/contactForm.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test';
 import { ContactActions } from './contactAction';
 import { LoginActions } from '../Login/LoginAction';
 import { LoginUsers } from '../../fixture/test-data';
+import { faker } from '@faker-js/faker';
 
 const OprationManager = LoginUsers.manager;
 const salesAgent = LoginUsers.sales;
@@ -44,6 +45,21 @@ test.describe('Contacts side Menu Tests - Remmi E2E', () => {
       process.env.E2E_MANAGER_OTP_SECRET!
     );
     await contact.verifyImageUploadFunctionalityNotDisplayed()
+  });
+
+  test('Test 4: Verify that the contact initials placeholder is displayed', async ({ page }) => {
+    const login = new LoginActions(page);
+    const contact = new ContactActions(page);
+
+    await login.login(
+      OprationManager.email!,
+      OprationManager.password!,
+      process.env.E2E_MANAGER_OTP_SECRET!
+    );
+
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    await contact.verifyContactInitialsPlaceholderDisplays(firstName, lastName);
   });
 });
    
