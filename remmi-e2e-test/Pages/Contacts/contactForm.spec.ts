65ae2a4ca831a48ec582d1ab5b5764935243f6b7
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-11-13 15:47:17 +0500

    Verify that all address fields are displayed correctly

--- remmi-e2e-test/Pages/Contacts/contactForm.spec.ts
+++ remmi-e2e-test/Pages/Contacts/contactForm.spec.ts
@@ -290,5 +290,17 @@ test.describe('Contacts side Menu Tests - Remmi E2E', () => {
     await contact.verifyAddressAutoFill('1600 Amphitheatre');
   });
 
+  test('Test 25: Verify that all address fields are displayed correctly', async ({ page }) => {
+    const login = new LoginActions(page);
+    const contact = new ContactActions(page);
+
+    await login.login(
+      OprationManager.email!,
+      OprationManager.password!,
+      process.env.E2E_MANAGER_OTP_SECRET!
+    );
+    await contact.verifyAllAddressFieldsDisplayed();
+  });
+
 });
   
