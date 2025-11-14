557e9427f249a6b136996f15cd7dcef505f69b65
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2025-11-14 10:27:38 +0500

    Search for a non existent tag in Tag Manager

--- remmi-e2e-test/Pages/Contacts/contactForm.spec.ts
+++ remmi-e2e-test/Pages/Contacts/contactForm.spec.ts
@@ -340,6 +340,18 @@ test.describe('Contacts side Menu Tests - Remmi E2E', () => {
     await contact.verifyCanAddNewTagType('Automation Testing');
   });
 
+  test('Test 28: Search for a non-existent tag in Tag Manager', async ({ page }) => {
+    const login = new LoginActions(page);
+    const contact = new ContactActions(page);
+
+    await login.login(
+      OprationManager.email!,
+      OprationManager.password!,
+      process.env.E2E_MANAGER_OTP_SECRET!
+    );
+    await contact.searchForNonExistentTag('NonExistentTag123');
+  });
+
 
 });
   
