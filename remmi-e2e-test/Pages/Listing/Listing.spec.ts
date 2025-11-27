b267d905f19bf8891bc0df04ee5794998fa73755
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2025-11-27 13:37:44 +0500

    Collapsing an expanded listing card

--- remmi-e2e-test/Pages/Listing/Listing.spec.ts
+++ remmi-e2e-test/Pages/Listing/Listing.spec.ts
@@ -173,6 +173,11 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.expandFirstContactCard();
   });
 
+  test('Test 32: Collapsing an expanded Listing card', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.collapseExpandedListingCard();
+  });
+
 });
 
 
