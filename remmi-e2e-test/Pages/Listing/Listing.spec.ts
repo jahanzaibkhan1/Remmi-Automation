af649451e0f0cc081efdca24c09a573a6b23d024
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-11-28 15:04:23 +0500

    Opening the contact form

--- remmi-e2e-test/Pages/Listing/Listing.spec.ts
+++ remmi-e2e-test/Pages/Listing/Listing.spec.ts
@@ -224,6 +224,11 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.switchToListView();
   });
 
+  test('Test 42: Opening the contact form', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.openListingForm();
+  });
+
 });
 
 
