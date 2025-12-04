3010b880a97921162fb5e0dab825d614de512607
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-12-04 09:44:53 +0500

    Filtering by a valid listing status in List View

--- remmi-e2e-test/Pages/Listing/Listingview.spec.ts
+++ remmi-e2e-test/Pages/Listing/Listingview.spec.ts
@@ -104,6 +104,12 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.resetFilters();
   });
 
+  test('Test 15: Filtering by a valid listing status in List View', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.filterByValidListingStatus('For Sale');
+    await listingActions.resetFilters();
+  });
+
 });
 
 
