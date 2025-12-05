6d89ae8be6b0615f4cf063f211b451648c2ea0aa
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-12-05 16:51:05 +0500

    fix locator

--- remmi-e2e-test/Pages/Listing/ListingLocator.ts
+++ remmi-e2e-test/Pages/Listing/ListingLocator.ts
@@ -136,7 +136,7 @@ export class ListingLocators {
     }
 
     dragHandle(): Locator {
-        return this.page.locator('.cdk-drag.column-item.custom-field-views').nth(0);
+        return this.page.locator('.cdk-drag.column-item.custom-field-views');
     }
     
     plusButton(): Locator {
