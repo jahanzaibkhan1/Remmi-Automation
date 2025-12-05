54e19717ecb40dd9f061988af5c0a56aa2366dc4
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-12-05 09:38:21 +0500

    Selecting multiple contract statuses in List View

--- remmi-e2e-test/Pages/Listing/Listingview.spec.ts
+++ remmi-e2e-test/Pages/Listing/Listingview.spec.ts
@@ -170,6 +170,12 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.resetFilters();
   });
 
+  test('Test 26: Selecting multiple contract statuses in List View', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.selectMultipleContractStatusesInListView();
+    await listingActions.resetFilters();
+  });
+
 });
 
 
