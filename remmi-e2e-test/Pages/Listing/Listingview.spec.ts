4a16206d7d58a3fc9e5007e246b3fdb053cd0cd2
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-12-08 17:17:47 +0500

    Applying a valid filter on a status

--- remmi-e2e-test/Pages/Listing/Listingview.spec.ts
+++ remmi-e2e-test/Pages/Listing/Listingview.spec.ts
@@ -277,6 +277,12 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.resetFilters();
   });
 
+  test('Test 44: Applying a valid filter on a status', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.applyvalidListingFilter('For Sale');
+    await listingActions.resetFilters();
+  });
+
 });
 
 
