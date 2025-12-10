7136a52b0a19c0d3883c912d3d013b693ac06d82
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2025-12-10 10:10:22 +0500

    Applying a filter and then quickly clicking Reset

--- remmi-e2e-test/Pages/Listing/Listingview.spec.ts
+++ remmi-e2e-test/Pages/Listing/Listingview.spec.ts
@@ -344,6 +344,12 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.resetFilters();
   });
 
+  test('Test 55: Applying a filter and then quickly clicking Reset', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.applyFilterAndQuickReset('For Sale');
+    await listingActions.resetFilters();
+  });
+
 
 });
 
