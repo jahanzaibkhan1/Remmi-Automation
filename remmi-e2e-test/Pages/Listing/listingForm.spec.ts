121bbd1f2e1f7f3df7d9d3a3c123668fe682b7c2
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2025-12-12 17:15:36 +0500

    Confirming previous listing  data copy

--- remmi-e2e-test/Pages/Listing/listingForm.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingForm.spec.ts
@@ -97,4 +97,10 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.selectPreviousListing();
   });
 
+  test('Test 15: Confirming previous listing data copy', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.navigateToListings();
+    await listingActions.previousListingCopy();
+  });
+
 });
