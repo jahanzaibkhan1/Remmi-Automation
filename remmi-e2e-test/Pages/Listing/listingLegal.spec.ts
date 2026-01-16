0c101e81138c61e806b2c574a2f0716df39356c9
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-01-16 10:54:36 +0500

    Verify that clicking the Add button in the Legal tab opens the contract popup in a new tab

---
+++ remmi-e2e-test/Pages/Listing/listingLegal.spec.ts
@@ -0,0 +1,29 @@
+import { test as base } from '@playwright/test';
+import { ListingActions } from './ListingAction';
+import * as path from 'path';
+
+const managerSessionPath = path.join(__dirname, '../../sessions/manager-session.json');
+const DASHBOARD_URL = process.env.DASHBOARD_URL || 'https://remmi-app-stage-ui.azurewebsites.net/dashboard';
+
+const test = base.extend<{ sessionPage: any }>({
+  sessionPage: [async ({ browser }, use) => {
+    const context = await browser.newContext({ storageState: managerSessionPath });
+    try {
+      const page = await context.newPage();
+      await page.goto(DASHBOARD_URL);
+      await use(page);
+    } finally {
+
+    }
+  }, { scope: 'worker' }]
+});
+
+
+test.describe('Listing side Menu Tests - Remmi E2E', () => {
+
+    test('Test 1: Verify that clicking the Add button in the Legal tab opens the contract popup in a new tab', async ({ sessionPage }) => {
+      const listingActions = new ListingActions(sessionPage);
+      await listingActions.verifyAddButtonInLegalTabOpensContractPopup();
+    });
+
+});
