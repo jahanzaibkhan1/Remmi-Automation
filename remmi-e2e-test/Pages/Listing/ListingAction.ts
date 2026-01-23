8ff810f15cecc450a9dd589784fbf514c8801cf9
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-01-23 16:35:16 +0500

    Fix issues

--- remmi-e2e-test/Pages/Listing/ListingAction.ts
+++ remmi-e2e-test/Pages/Listing/ListingAction.ts
@@ -12712,10 +12712,6 @@ export class ListingActions {
         const downloadLocator = this.page.locator('img.img-hub2[src*="propertyImage"]').last();
         await downloadLocator.scrollIntoViewIfNeeded();
         await expect(downloadLocator).toBeVisible({timeout:20000});
-<<<<<<< HEAD
-        // Check for download element in the same context
-=======
->>>>>>> d20df2b (Verify double clicking a file opens preview)
         await downloadLocator.click();
 
         const downloadIcon = this.page.locator('.p-element.mr-3.pi.pi-download').first();
@@ -12805,7 +12801,6 @@ export class ListingActions {
         }
     }
 
-<<<<<<< HEAD
     /**
      * Verify that right clicking a folder shows options for Share, Rename, Make a Copy, and Remove
      */
@@ -12861,9 +12856,6 @@ export class ListingActions {
         }
 
     }
-=======
-
->>>>>>> d20df2b (Verify double clicking a file opens preview)
 
 }
 
