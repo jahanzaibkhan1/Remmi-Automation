25e35978895c3e449c3b1d5b885145b13f953b99
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2026-01-26 17:33:09 +0500

    Verify that clicking a file shows action options

--- remmi-e2e-test/Pages/Listing/listingDocument.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingDocument.spec.ts
@@ -156,4 +156,9 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
         await listingActions.verifySharedStaffTeamHasProfileImage();
     });
 
+    test('Test 27: Verify that clicking a file shows action options', async ({ sessionPage }) => {
+        const listingActions = new ListingActions(sessionPage);
+        await listingActions.verifyImageFileShowsActionOptions();
+    });
+
 });
