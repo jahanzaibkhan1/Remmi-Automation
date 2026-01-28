2ed7f86e70157f09434c95441c88641240b1fe59
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-01-28 11:58:14 +0500

    Verify clicking on a folder tab navigates to it

--- remmi-e2e-test/Pages/Listing/listingDocument.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingDocument.spec.ts
@@ -227,5 +227,10 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
         const listingActions = new ListingActions(sessionPage);
         await listingActions.verifyFolderNamesAppearAsNavigationTabs();
     });
+
+    test('Test 41: Verify clicking on a folder tab navigates to it', async ({ sessionPage }) => {
+        const listingActions = new ListingActions(sessionPage);
+        await listingActions.verifyClickingFolderTabNavigatesToFolder();
+    });
     
 });
