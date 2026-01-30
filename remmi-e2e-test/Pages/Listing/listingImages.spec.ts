8bb74d7759a72514acfc3e79f1a1690a1fb5c510
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2026-01-30 09:26:02 +0500

    Verify that uploading an image through the Edit popup works

--- remmi-e2e-test/Pages/Listing/listingImages.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingImages.spec.ts
@@ -210,4 +210,9 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.verifyImageMakeCopyCreatesCopy();
   });
 
+  test('Test 37: Verify that uploading an image through the Edit popup works', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.verifyUploadImageThroughEditPopupWorks();
+  });
+
 });
