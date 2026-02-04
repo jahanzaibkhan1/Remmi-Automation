ad7947c91cb728463da0f9da596f036a607c0c68
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-02-04 15:54:13 +0500

    Verify date and time is displayed on each stream card

--- remmi-e2e-test/Pages/Listing/listingStream.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingStream.spec.ts
@@ -82,4 +82,10 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
       await listingActions.verifyStreamCardAppearsForListingCreation();
     });
 
+    test('Test 13: Verify date and time is displayed on each stream card', async ({ sessionPage }) => {
+      const listingActions = new ListingActions(sessionPage);
+      await listingActions.verifyDateTimeDisplayedOnEachStreamCard();
+    });
+
+
 });
