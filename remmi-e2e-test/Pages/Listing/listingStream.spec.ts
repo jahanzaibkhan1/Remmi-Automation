63c5868661959c4c801abad00d2eb28154182053
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-02-05 16:19:27 +0500

    Verify page refresh does not remove stream cards

--- remmi-e2e-test/Pages/Listing/listingStream.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingStream.spec.ts
@@ -144,5 +144,10 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
       await listingActions.verifySearchFunctionalityInStreamTab('Jahanzaib Xenex');
     });
 
+    test('Test 25: Verify page refresh does not remove stream cards', async ({ sessionPage }) => {
+      const listingActions = new ListingActions(sessionPage);
+      await listingActions.verifyStreamCardsPersistAfterRefresh();
+    });
+
 
 });
