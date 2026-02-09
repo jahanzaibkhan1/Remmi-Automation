c91785f2b07ee89944f14d06ca1b2ee9c63a3d72
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-02-09 10:57:26 +0500

    Verify invalid time/date formats do not affect stream tab

--- remmi-e2e-test/Pages/Listing/listingStream.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingStream.spec.ts
@@ -164,5 +164,10 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
       await listingActions.verifyInfiniteScrollingInStreamTab();
     });
 
+    test('Test 29: Verify invalid time/date formats do not affect stream tab', async ({ sessionPage }) => {
+      const listingActions = new ListingActions(sessionPage);
+      await listingActions.verifyInspectionCannotAddPastDate();
+    });
+
 
 });
