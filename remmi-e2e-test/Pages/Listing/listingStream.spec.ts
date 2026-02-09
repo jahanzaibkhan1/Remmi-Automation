8653dd09df248733ecb6974848d4250808a0f492
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2026-02-09 10:48:58 +0500

    Verify excessive scrolling does not break the UI

--- remmi-e2e-test/Pages/Listing/listingStream.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingStream.spec.ts
@@ -159,5 +159,10 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
       await listingActions.verifyErrorHandlingForFailedListingCreation();
     });
 
+    test('Test 28: Verify excessive scrolling does not break the UI', async ({ sessionPage }) => {
+      const listingActions = new ListingActions(sessionPage);
+      await listingActions.verifyInfiniteScrollingInStreamTab();
+    });
+
 
 });
