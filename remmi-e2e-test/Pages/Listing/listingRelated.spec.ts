9eba5bd22c647e64b407634cff04c58452ff8137
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2026-02-13 14:49:35 +0500

    Verify that a relationship tag can be removed from a related contact

--- remmi-e2e-test/Pages/Listing/listingRelated.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingRelated.spec.ts
@@ -85,4 +85,9 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.verifyDuplicateRelationshipTagsCannotBeAdded();
   });
 
+  test('Test 14: Verify that a relationship tag can be removed from a related contact', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.verifyRelationshipTagCanBeRemoved();
+  });
+
 });
