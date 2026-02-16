f5060200c82381be36d4f6327141505912e65d3c
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2026-02-16 12:11:20 +0500

    Verify that total associated contact count updates correctly

--- remmi-e2e-test/Pages/Listing/listingRelated.spec.ts
+++ remmi-e2e-test/Pages/Listing/listingRelated.spec.ts
@@ -90,4 +90,9 @@ test.describe('Listing side Menu Tests - Remmi E2E', () => {
     await listingActions.verifyRelationshipTagCanBeRemoved();
   });
 
+  test('Test 15: Verify that total associated contact count updates correctly', async ({ sessionPage }) => {
+    const listingActions = new ListingActions(sessionPage);
+    await listingActions.verifyAssociatedContactCountUpdatesCorrectly();
+  });
+
 });
