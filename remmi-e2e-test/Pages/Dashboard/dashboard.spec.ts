2ea33b1f23e0fc4176c5201d858239965cb3a2cc
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-03-09 16:04:22 +0500

    Verify new row addition

--- remmi-e2e-test/Pages/Dashboard/dashboard.spec.ts
+++ remmi-e2e-test/Pages/Dashboard/dashboard.spec.ts
@@ -66,5 +66,10 @@ test.describe("Dashboard Module Boards Display", () => {
     const dashboard = new DashboardAction(sessionPage);
     await dashboard.verifyRemovingRowRemovesAllBoards();
   });
+
+  test('Verify new row addition', async ({ sessionPage }) => {
+    const dashboard = new DashboardAction(sessionPage);
+    await dashboard.verifyNewRowAddition();
+  });
   
 });
