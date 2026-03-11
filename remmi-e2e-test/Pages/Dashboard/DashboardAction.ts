73da360f8d86922b3cb1f4cef933221210563193
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-03-11 14:46:45 +0500

    Verify listing details on desktop

--- remmi-e2e-test/Pages/Dashboard/DashboardAction.ts
+++ remmi-e2e-test/Pages/Dashboard/DashboardAction.ts
@@ -959,4 +959,22 @@ export class DashboardAction {
     await leftArrow.click();
   }
 
+/**
+ * Verify listing details on desktop
+ */
+async verifyListingDetailsOnDashboardDesktop() {
+  await this.verifyDashboardLoaded();
+  const propertyCard = this.page.locator('ul.listing li').nth(1);
+  await propertyCard.scrollIntoViewIfNeeded();
+  await propertyCard.waitFor({ state: 'visible' });
+  const headerImage = propertyCard.locator('img');
+  await headerImage.waitFor({ state: 'visible' });
+  const saleTag = propertyCard.locator('button');
+  await saleTag.waitFor({ state: 'visible' });
+  const heading = propertyCard.locator('h4');
+  await heading.scrollIntoViewIfNeeded();
+  await heading.waitFor({ state: 'visible' });
+}
+
+
 }
--- remmi-e2e-test/Pages/Dashboard/dashboard.spec.ts
+++ remmi-e2e-test/Pages/Dashboard/dashboard.spec.ts
@@ -87,5 +87,10 @@ test.describe("Dashboard Module Boards Display", () => {
     const dashboard = new DashboardAction(sessionPage);
     await dashboard.verifyListingCarouselArrows();
   });
+
+  test('Verify listing details on desktop', async ({ sessionPage }) => {
+    const dashboard = new DashboardAction(sessionPage);
+    await dashboard.verifyListingDetailsOnDashboardDesktop();
+  });
   
 });
