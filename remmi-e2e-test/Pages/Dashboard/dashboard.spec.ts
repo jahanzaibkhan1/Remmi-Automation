56260f3ba1262b251b0cf15357aa98d50a3f816d
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2026-03-13 16:40:02 +0500

    Verify that Report board on the Dashboard shows correct data

--- remmi-e2e-test/Pages/Dashboard/dashboard.spec.ts
+++ remmi-e2e-test/Pages/Dashboard/dashboard.spec.ts
@@ -219,5 +219,10 @@ test.describe("Dashboard Module Boards Display", () => {
     const dashboard = new DashboardAction(sessionPage);
     await dashboard.verifyContractBoardShowsCorrectData();
   });
-  
+
+  test('Verify that Report board on the Dashboard shows correct data', async ({ sessionPage }) => {
+    const dashboard = new DashboardAction(sessionPage);
+    await dashboard.verifyReportsBoardIsDisplayedCorrectly();
+  });
+
 });
