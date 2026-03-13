cd17e83197fc0dc89524fa1aa4f22a3016fdc71c
Muhammad Jahanzaib <90091720+jahanzaibkhan1@users.noreply.github.com>
2026-03-13 14:32:12 +0500

    Verify loading of listing thumbnails

--- remmi-e2e-test/Pages/Dashboard/dashboard.spec.ts
+++ remmi-e2e-test/Pages/Dashboard/dashboard.spec.ts
@@ -185,5 +185,10 @@ test.describe("Dashboard Module Boards Display", () => {
     await dashboard.verifyEOIBoardDisplaysCorrectData();
   });
 
+  test('Verify loading of listing thumbnails', async ({ sessionPage }) => {
+    const dashboard = new DashboardAction(sessionPage);
+    await dashboard.verifyLoadingOfListingThumbnails();
+  });
+
   
 });
