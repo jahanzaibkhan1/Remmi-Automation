3339c2f395a2abb46e139cb49a4137deb4e33cbb
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-04-22 14:34:43 +0500

    Verify active tab filtering

--- remmi-e2e-test/Pages/Projects/projectListView.spec.ts
+++ remmi-e2e-test/Pages/Projects/projectListView.spec.ts
@@ -28,4 +28,9 @@ test.describe('Projects - List View Tests', () => {
         const project = new ProjectActions(sessionPage);
         await project.verifyProjectSearchWithInvalidNameInListView('NoSuchProjectABC123');
     });
+
+    test('Verify active tab filtering in List View', async ({ sessionPage }) => {
+        const project = new ProjectActions(sessionPage);
+        await project.verifyActiveTabFilteringInListView();
+    });
 });
