4ea71f17060d25981dcc67aee26ad03fc772e34b
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-04-22 15:22:37 +0500

    Select single project manager

--- remmi-e2e-test/Pages/Projects/projectListView.spec.ts
+++ remmi-e2e-test/Pages/Projects/projectListView.spec.ts
@@ -38,4 +38,10 @@ test.describe('Projects - List View Tests', () => {
         const project = new ProjectActions(sessionPage);
         await project.verifyInactiveTabFilteringInListView();
     });
+
+    test('Select single project manager in List View', async ({ sessionPage }) => {
+        const project = new ProjectActions(sessionPage);
+        await project.selectSingleProjectManagerInListView('Jahanzaib Xenex');
+    });
+    
 });
