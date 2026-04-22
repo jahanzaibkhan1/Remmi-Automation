d03d53cfcb695c7c4605c8db622110d0e0942e51
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-04-22 16:28:19 +0500

    Create a new custom view

--- remmi-e2e-test/Pages/Projects/projectListView.spec.ts
+++ remmi-e2e-test/Pages/Projects/projectListView.spec.ts
@@ -68,5 +68,11 @@ test.describe('Projects - List View Tests', () => {
         const project = new ProjectActions(sessionPage);
         await project.verifyDefaultViewPopupOpensInListView();
     });
+
+    test('Create a new custom view in List View', async ({ sessionPage }) => {
+        const project = new ProjectActions(sessionPage);
+        const uniqueViewName = `Test View`;
+        await project.createNewCustomViewInListView(uniqueViewName);
+    });
    
 });
