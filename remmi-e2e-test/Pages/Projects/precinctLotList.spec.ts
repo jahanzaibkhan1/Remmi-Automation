28cf3658e322ffb4e941911894e696749e0dd146
Muhammad Jahanzaib <jahanzaib@xenex-media.com.au>
2026-05-07 08:53:53 +0500

    Open Status dropdown

--- remmi-e2e-test/Pages/Projects/precinctLotList.spec.ts
+++ remmi-e2e-test/Pages/Projects/precinctLotList.spec.ts
@@ -47,44 +47,48 @@ test.describe('Lot List Page', () => {
         await new ProjectActions(sessionPage).selectMultipleProjectsInLotDropdown(['Adb', 'Automation Testing']);
     });
 
-    test('TC_07: Use Select All in Project dropdown', async ({ sessionPage }) => {
+    test('TC_08: Use Select All in Project dropdown', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).useSelectAllInProjectDropdown();
     });
 
-    test('TC_08: Use Deselect All in Project dropdown', async ({ sessionPage }) => {
+    test('TC_09: Use Deselect All in Project dropdown', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).useDeselectAllInProjectDropdown();
     });
 
-    test('TC_09: Remove selected project tag from Project dropdown', async ({ sessionPage }) => {
+    test('TC_010: Remove selected project tag from Project dropdown', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).removeSelectedProjectTagInLotDropdown('Adb');
     });
 
-    test('TC_10: Open Bed dropdown', async ({ sessionPage }) => {
+    test('TC_11: Open Bed dropdown', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).openAndAssertBedDropdown();
     });
 
-    test('TC_11: Search bed number in Bed dropdown', async ({ sessionPage }) => {
+    test('TC_12: Search bed number in Bed dropdown', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).searchBedInBedDropdown('2');
     });
 
-    test('TC_12: Select single bed number', async ({ sessionPage }) => {
+    test('TC_13: Select single bed number', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).selectSingleBedInDropdown('2');
     });
 
-    test('TC_13: Select multiple bed numbers', async ({ sessionPage }) => {
+    test('TC_14: Select multiple bed numbers', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).selectMultipleBedsInDropdown(['1', '2']);
     });
 
-    test('TC_14: Use Select All in Bed dropdown', async ({ sessionPage }) => {
+    test('TC_15: Use Select All in Bed dropdown', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).useSelectAllInBedDropdown();
     });
 
-    test('TC_15: Use Deselect All in Bed dropdown', async ({ sessionPage }) => {
+    test('TC_16: Use Deselect All in Bed dropdown', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).useDeselectAllInBedDropdown();
     });
 
-    test('TC_16: Remove bed tag from Bed dropdown', async ({ sessionPage }) => {
+    test('TC_17: Remove bed tag from Bed dropdown', async ({ sessionPage }) => {
         await new ProjectActions(sessionPage).removeSelectedBedTagInBedDropdown('2');
+    });
+
+    test('TC_18: Open Status dropdown', async ({ sessionPage }) => {
+        await new ProjectActions(sessionPage).openAndAssertStatusDropdown();
     });
     
 });
