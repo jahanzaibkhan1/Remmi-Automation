31e2b2907cdf7845985ad067161aa74978c81d71
Muhammad Jahanzaib <jahanzaib@xenex-media.com.au>
2026-05-08 08:52:04 +0500

    Validate deselected tag is removed from list

--- remmi-e2e-test/Pages/Projects/projectAction.ts
+++ remmi-e2e-test/Pages/Projects/projectAction.ts
@@ -5635,4 +5635,21 @@ export class ProjectActions {
         await expect(this.selectedProjectTagByName(projectName)).not.toBeVisible({ timeout: ProjectActions.TIMEOUT_DEFAULT });
         await this.cleanupAfterLotTest();
     }
+
+    /**
+     * Validate that a deselected project tag is removed from the tag list
+     */
+    async validateDeselectedTagIsRemoved(projectName: string): Promise<void> {
+        await this.navigateToLotTabInPrecinct();
+        await this.resetAndAssertLotRowVisible();
+        await this.openProjectDropdownAndSearch(projectName);
+        await this.projectDropdownOptions.first().click();
+        await this.closeDropdown();
+        await expect(this.selectedProjectTagByName(projectName)).toBeVisible({ timeout: ProjectActions.TIMEOUT_DEFAULT });
+        await this.openProjectDropdownAndSearch(projectName);
+        await this.projectDropdownOptions.first().click();
+        await this.closeDropdown();
+        await expect(this.selectedProjectTagByName(projectName)).not.toBeVisible({ timeout: ProjectActions.TIMEOUT_DEFAULT });
+        await this.cleanupAfterLotTest();
+    }
 }
