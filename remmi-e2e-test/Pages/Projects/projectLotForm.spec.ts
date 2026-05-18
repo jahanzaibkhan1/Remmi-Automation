63d255985dcd2600233459f917a743c57d9f0d7a
Muhammad Jahanzaib <jahanzaibkorai75@gmail.com>
2026-05-18 12:30:17 +0500

    Verify project can be changed

--- remmi-e2e-test/Pages/Projects/projectLotForm.spec.ts
+++ remmi-e2e-test/Pages/Projects/projectLotForm.spec.ts
@@ -44,12 +44,8 @@ test.describe('Project Lot Form Tests', () => {
         await new ProjectActions(sessionPage).verifyApartmentDetailAndHistoryTabsVisible('Automation', 'Automation Lot');
     });
 
-    test('TC_07: Verify clicking Project field opens Project list popup', async ({ sessionPage }) => {
-        await new ProjectActions(sessionPage).verifyProjectFieldOpensProjectList('Automation', 'Automation Lot');
-    });
-
-    test('TC_08: Verify close icon closes Project Tag popup', async ({ sessionPage }) => {
-        await new ProjectActions(sessionPage).verifyCloseIconClosesProjectList('Automation', 'Automation Lot');
+    test('TC_07: Verify project dropdown is auto-filled', async ({ sessionPage }) => {
+        await new ProjectActions(sessionPage).verifyProjectsDropdownAutoFilled('Automation', 'Automation Lot');
     });
 
 });
